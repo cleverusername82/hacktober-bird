edec9c0ef96c5b1fc27c558016e13ccdcdfaf410
Your Name <cleverusername82@gmail.com>
2020-10-03 19:11:15 -0400

    Separate script

--- script.js
+++ script.js
@@ -1,53 +1,127 @@
-var wall = document.getElementById("wall");
-var hole = document.getElementById("hole");
-var character = document.getElementById("character");
-var jumping = 0;
-var counter = 0;
+var player;
+var myObstacles = [];
+var score;
 
-hole.addEventListener("animationiteration", () => {
-  var random = -(Math.random() * 300 + 150);
-  hole.style.top = random + "px";
-  counter++;
-});
-setInterval(function () {
-  var characterTop = parseInt(
-    window.getComputedStyle(character).getPropertyValue("top")
-  );
-  if (jumping == 0) {
-    character.style.top = characterTop + 3 + "px";
-  }
-  var wallLeft = parseInt(
-    window.getComputedStyle(wall).getPropertyValue("left")
-  );
-  var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-  var cTop = -(500 - characterTop);
-  if (
-    characterTop > 480 ||
-    (wallLeft < 20 &&
-      wallLeft > -50 &&
-      (cTop < holeTop || cTop > holeTop + 130))
-  ) {
-    alert("Game over. Score: " + (counter - 1));
-    character.style.top = 100 + "px";
-    counter = 0;
-  }
-}, 10);
+function startGame() {
+  player = new component(30, 30, "red", 10, 120);
+  player.gravity = 0.1;
+  score = new component("30px", "Arial", "black", 280, 40, "text");
+  gameArea.start();
+}
 
-function jump() {
-  jumping = 1;
-  let jumpCount = 0;
-  var jumpInterval = setInterval(function () {
-    var characterTop = parseInt(
-      window.getComputedStyle(character).getPropertyValue("top")
+var gameArea = {
+  canvas: document.createElement("canvas"),
+  start: function () {
+    this.canvas.width = 800;
+    this.canvas.height = 400;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.frameNo = 0;
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear: function () {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+};
+
+function component(width, height, color, x, y, type) {
+  this.type = type;
+  this.score = 0;
+  this.width = width;
+  this.height = height;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.x = x;
+  this.y = y;
+  this.gravity = 0;
+  this.gravitySpeed = 0;
+  this.update = function () {
+    ctx = gameArea.context;
+    if (this.type == "text") {
+      ctx.font = this.width + " " + this.height;
+      ctx.fillStyle = color;
+      ctx.fillText(this.text, this.x, this.y);
+    } else {
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  };
+  this.newPos = function () {
+    this.gravitySpeed += this.gravity;
+    this.x += this.speedX;
+    this.y += this.speedY + this.gravitySpeed;
+    this.hitBottom();
+  };
+  this.hitBottom = function () {
+    var rockbottom = gameArea.canvas.height - this.height;
+    if (this.y > rockbottom) {
+      this.y = rockbottom;
+      this.gravitySpeed = 0;
+    }
+  };
+  this.crashWith = function (otherobj) {
+    var myleft = this.x;
+    var myright = this.x + this.width;
+    var mytop = this.y;
+    var mybottom = this.y + this.height;
+    var otherleft = otherobj.x;
+    var otherright = otherobj.x + otherobj.width;
+    var othertop = otherobj.y;
+    var otherbottom = otherobj.y + otherobj.height;
+    var crash = true;
+    if (
+      mybottom < othertop ||
+      mytop > otherbottom ||
+      myright < otherleft ||
+      myleft > otherright
+    ) {
+      crash = false;
+    }
+    return crash;
+  };
+}
+
+function updateGameArea() {
+  var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+  for (i = 0; i < myObstacles.length; i += 1) {
+    if (player.crashWith(myObstacles[i])) {
+      return;
+    }
+  }
+  gameArea.clear();
+  gameArea.frameNo += 1;
+  if (gameArea.frameNo == 1 || everyinterval(150)) {
+    x = gameArea.canvas.width;
+    minHeight = 20;
+    maxHeight = 200;
+    height = Math.floor(
+      Math.random() * (maxHeight - minHeight + 1) + minHeight
     );
-    if (characterTop > 6 && jumpCount < 15) {
-      character.style.top = characterTop - 5 + "px";
-    }
-    if (jumpCount > 20) {
-      clearInterval(jumpInterval);
-      jumping = 0;
-      jumpCount = 0;
-    }
-    jumpCount++;
-  }, 10);
+    minGap = 50;
+    maxGap = 200;
+    gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+    myObstacles.push(new component(20, height, "#3e8e41", x, 0));
+    myObstacles.push(
+      new component(20, x - height - gap, "#3e8e41", x, height + gap)
+    );
+  }
+  for (i = 0; i < myObstacles.length; i += 1) {
+    myObstacles[i].x += -1;
+    myObstacles[i].update();
+  }
+  score.text = "SCORE: " + gameArea.frameNo;
+  score.update();
+  player.newPos();
+  player.update();
+}
+
+function everyinterval(n) {
+  if ((gameArea.frameNo / n) % 1 == 0) {
+    return true;
+  }
+  return false;
+}
+
+function accelerate(n) {
+  player.gravity = n;
 }
